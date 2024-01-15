4306278c19a00811c30e7de825978676911501dc
kanugurajesh <kanugurajesh3@gmail.com>
2024-01-15 21:49:37 +0530

    Adding the final product

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  publicRoutes: ["/", "/Home", "/Contact", "/api/uploadthing"],
+  publicRoutes: ["/", "/Home", "/Contact", "/api/uploadthing", "/api/send"],
 });
  
 export const config = {
