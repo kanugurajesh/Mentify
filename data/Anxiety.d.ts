d5977ec294ddce3323dc19fc9219252a9a7744b0
kanugurajesh <kanugurajesh3@gmail.com>
2024-01-16 13:29:41 +0530

    Updating the quizzes

--- data/Anxiety.d.ts
+++ data/Anxiety.d.ts
@@ -3,100 +3,100 @@ export const data = {
     {
       "question": "How often do you find yourself experiencing anxiety in daily situations?",
       "options": [
-        "Rarely",
-        "Occasionally",
-        "Frequently",
-        "Almost always"
+        "Rarely+0",
+        "Occasionally+1",
+        "Frequently+2",
+        "Almost always+3"
       ],
       "correctOption": "Rarely"
     },
     {
       "question": "Do you tend to express your anxiety outwardly or keep it to yourself?",
       "options": [
-        "Express outwardly",
-        "Keep to myself",
-        "Varies depending on the situation",
-        "I don't experience anxiety"
+        "Express outwardly+3",
+        "Keep to myself+1",
+        "Varies depending on the situation+2",
+        "I don't experience anxiety+0"
       ],
-      "correctOption": "Keep to myself"
+      "correctOption": "I don't experience anxiety"
     },
     {
       "question": "What triggers your anxiety the most?",
       "options": [
-        "Uncertainty",
-        "Social situations",
-        "Performance expectations",
-        "Other (please specify)"
+        "Uncertainty+1",
+        "Social situations+3",
+        "Performance expectations+2",
+        "Feeling threatened+0"
       ],
-      "correctOption": "Uncertainty"
+      "correctOption": "Feeling threatened+0"
     },
     {
       "question": "How would you describe the intensity of your anxiety when it occurs?",
       "options": [
-        "Mild",
-        "Moderate",
-        "Severe",
-        "Extreme"
+        "Mild+0",
+        "Moderate+1",
+        "Severe+2",
+        "Extreme+3"
       ],
       "correctOption": "Mild"
     },
     {
       "question": "Do you actively seek methods to manage and control your anxiety?",
       "options": [
-        "Yes, regularly",
-        "Sometimes",
-        "Rarely",
-        "No, not at all"
+        "Yes, regularly+3",
+        "Sometimes+2",
+        "Rarely+1",
+        "No, not at all+0"
       ],
       "correctOption": "Yes, regularly"
     },
     {
       "question": "Have others expressed concern about your anxiety behavior?",
       "options": [
-        "Yes, frequently",
-        "Occasionally",
-        "Rarely",
-        "No, never"
+        "Yes, frequently+3",
+        "Occasionally+2",
+        "Rarely+1",
+        "No, never+0"
       ],
-      "correctOption": "Occasionally"
+      "correctOption": "No, never"
     },
     {
       "question": "Do you feel regret or guilt after experiencing anxiety?",
       "options": [
-        "Always",
-        "Sometimes",
-        "Rarely",
-        "Never"
+        "Always+0",
+        "Sometimes+1",
+        "Rarely+2",
+        "Never+3"
       ],
-      "correctOption": "Sometimes"
+      "correctOption": "Always"
     },
     {
       "question": "Have you sought professional help or counseling for anxiety-related issues?",
       "options": [
-        "Yes",
-        "Considering it",
-        "No, not yet",
-        "No, and I don't plan to"
+        "Yes+0",
+        "Considering it+1",
+        "No, not yet+2",
+        "No, and I don't plan to+3"
       ],
-      "correctOption": "Considering it"
+      "correctOption": "Yes"
     },
     {
       "question": "How would you rate the impact of anxiety on your relationships and daily life?",
       "options": [
-        "Low impact",
-        "Moderate impact",
-        "High impact",
-        "Very high impact"
+        "Low impact+0",
+        "Moderate impact+1",
+        "High impact+2",
+        "Very high impact+3"
       ],
-      "correctOption": "Moderate impact"
+      "correctOption": "Low impact"
     },
     {
       "question": "Do you believe that addressing and managing anxiety is important for overall well-being?",
       "options": [
-        "Strongly agree",
-        "Agree",
-        "Neutral",
-        "Disagree"
+        "Strongly agree+0",
+        "Agree+1",
+        "Neutral+2",
+        "Disagree+3"
       ],
       "correctOption": "Strongly agree"
     }
