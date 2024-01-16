c0adc365b2603c76a7d7f3986f470d3db938d187
kanugurajesh <kanugurajesh3@gmail.com>
2024-01-16 14:14:47 +0530

    Adding quiz changes

--- data/Depression.d.ts
+++ data/Depression.d.ts
@@ -3,100 +3,100 @@ export const data = {
     {
       "question": "How would you describe your overall mood on a typical day?",
       "options": [
-        "Generally positive",
-        "Neutral",
-        "Occasionally sad",
-        "Frequently sad or low"
+        "Generally positive+0",
+        "Neutral+1",
+        "Occasionally sad+2",
+        "Frequently sad or low+3"
       ],
       "correctOption": "Generally positive"
     },
     {
       "question": "Do you often experience feelings of hopelessness or helplessness?",
       "options": [
-        "Rarely or never",
-        "Occasionally",
-        "Frequently",
-        "Almost always"
+        "Rarely or never+0",
+        "Occasionally+1",
+        "Frequently+2",
+        "Almost always+3"
       ],
       "correctOption": "Rarely or never"
     },
     {
       "question": "Have you noticed changes in your appetite, either overeating or loss of appetite?",
       "options": [
-        "No changes",
-        "Increased appetite",
-        "Decreased appetite",
-        "Fluctuations between increased and decreased appetite"
+        "No changes+0",
+        "Increased appetite+1",
+        "Decreased appetite+2",
+        "Fluctuations between increased and decreased appetite+3"
       ],
       "correctOption": "No changes"
     },
     {
       "question": "How is your sleep pattern affected? Do you experience difficulty sleeping or oversleeping?",
       "options": [
-        "No sleep issues",
-        "Difficulty sleeping",
-        "Oversleeping",
-        "Fluctuations between difficulty and oversleeping"
+        "No sleep issues+0",
+        "Difficulty sleeping+1",
+        "Oversleeping+2",
+        "Fluctuations between difficulty and oversleeping+3"
       ],
       "correctOption": "No sleep issues"
     },
     {
       "question": "Do you often feel fatigued or lacking in energy?",
       "options": [
-        "Rarely or never",
-        "Occasionally",
-        "Frequently",
-        "Almost always"
+        "Rarely or never+0",
+        "Occasionally+1",
+        "Frequently+2",
+        "Almost always+3"
       ],
       "correctOption": "Rarely or never"
     },
     {
       "question": "Have you lost interest or pleasure in activities that you used to enjoy?",
       "options": [
-        "No loss of interest",
-        "Occasional loss of interest",
-        "Frequent loss of interest",
-        "Complete loss of interest"
+        "No loss of interest+0",
+        "Occasional loss of interest+1",
+        "Frequent loss of interest+2",
+        "Complete loss of interest+3"
       ],
       "correctOption": "No loss of interest"
     },
     {
       "question": "Have you sought professional help or been diagnosed with depression?",
       "options": [
-        "Yes",
-        "Considering it",
-        "No, not yet",
-        "No, and I don't plan to"
+        "Yes+0",
+        "Considering it+1",
+        "No, not yet+2",
+        "No, and I don't plan to+3"
       ],
-      "correctOption": "Considering it"
+      "correctOption": "Yes"
     },
     {
       "question": "Do you find it challenging to concentrate or make decisions?",
       "options": [
-        "No challenges",
-        "Slight challenges",
-        "Moderate challenges",
-        "Severe challenges"
+        "No challenges+0",
+        "Slight challenges+1",
+        "Moderate challenges+2",
+        "Severe challenges+3"
       ],
       "correctOption": "No challenges"
     },
     {
       "question": "Have you experienced a persistent feeling of sadness or emptiness?",
       "options": [
-        "Rarely or never",
-        "Occasionally",
-        "Frequently",
-        "Almost always"
+        "Rarely or never+0",
+        "Occasionally+1",
+        "Frequently+2",
+        "Almost always+3"
       ],
       "correctOption": "Rarely or never"
     },
     {
       "question": "Do you believe that addressing and managing mood and emotional well-being is important for overall well-being?",
       "options": [
-        "Strongly agree",
-        "Agree",
-        "Neutral",
-        "Disagree"
+        "Strongly agree+0",
+        "Agree+1",
+        "Neutral+2",
+        "Disagree+3"
       ],
       "correctOption": "Strongly agree"
     }
