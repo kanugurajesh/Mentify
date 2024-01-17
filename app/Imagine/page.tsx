54e173c240676a8c5853ce4d199511bd995a2538
kanugurajesh <kanugurajesh3@gmail.com>
2024-01-17 12:58:37 +0530

    Adding the swap iamge ai changes

--- app/Imagine/page.tsx
+++ app/Imagine/page.tsx
@@ -8,214 +8,203 @@ import { UploadButton } from "@/utils/uploadthing";
 import Link from "next/link";
 import toast, { Toaster } from "react-hot-toast";
 import { BeatLoader } from "react-spinners";
-import styles from "@/styles/swapImage.module.css";
+import styles from "@/styles/Root.module.css";
 
 export default function Home() {
-  const [email, setEmail] = useState<string>("");
-  const [gender, setGender] = useState<string>("");
-  const [userPrompt, setUserPrompt] = useState<string>("");
-  const [click, setClick] = useState<boolean>(false);
-  const [selectedFile, setSelectedFile] = useState<string>();
-  const [imageURl, setImageURl] = useState<string>("");
-  const [emailSent, setEmailSent] = useState<boolean>(false);
-  const [receivedEmail, setReceivedEmail] = useState<boolean>(false);
 
-  const handleSubmit = async (e: any) => {
-    e.preventDefault();
-    try {
-      if (!selectedFile) {
-        return toast.error("Please upload a picture");
-      }
+    const [email, setEmail] = useState<string>("");
+    const [gender, setGender] = useState<string>("");
+    const [userPrompt, setUserPrompt] = useState<string>("");
+    const [click, setClick] = useState<boolean>(false);
+    const [selectedFile, setSelectedFile] = useState<string>();
+    const [imageURl, setImageURl] = useState<string>("");
+    const [emailSent, setEmailSent] = useState<boolean>(false);
+    const [receivedEmail, setReceivedEmail] = useState<boolean>(false);
 
-      const body = {
-        email: email,
-        gender: gender,
-        userPrompt: userPrompt,
-        selectedFile: selectedFile,
-      };
+    const handleSubmit = async (e: any) => {
+        e.preventDefault();
+        try {
 
-      // 👇🏻 post data to server's endpoint
-      const data = await fetch("/api/generate", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(body),
-      });
+            if (!selectedFile) {
+                return toast.error("Please upload a picture");
+            }
 
-      const response = await data.json();
+            const body = {
+                email: email,
+                gender: gender,
+                userPrompt: userPrompt,
+                selectedFile: selectedFile,
+            }
 
-      if (response.error) {
-        return toast.error(response.error);
-      }
+            // 👇🏻 post data to server's endpoint
+            const data = await fetch("/api/generate", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(body),
+            });
 
-      // 👇🏻 set image url
-      setImageURl(response.imageURl);
-    } catch (err) {
-      console.error({ err });
-    }
-  };
+            const response = await data.json();
 
-  const handleEmailReceived = () => {
-    if (!receivedEmail) {
-      toast.success("Email receive is enabled");
-    } else {
-      toast.error("Email receive is disabled");
-    }
+            if (response.error) {
+                return toast.error(response.error);
+            }
 
-    setReceivedEmail(!receivedEmail);
-  };
+            // 👇🏻 set image url
+            setImageURl(response.imageURl);
 
-  useEffect(() => {
-    if (imageURl) {
-      setEmail("");
-      setGender("");
-      setUserPrompt("");
-      setSelectedFile("");
-    }
-
-    const sendEmail = async () => {
-      const emailRes = await fetch(`/api/send`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          email: email,
-          imageURl: imageURl,
-        }),
-      });
-
-      const emailResponse = await emailRes.json();
-
-      if (emailResponse.error) {
-        console.error({ emailResponse });
-        return toast.error(emailResponse.error);
-      }
-
-      setEmailSent(true);
-
-      toast.success("Email sent successfully!");
+        } catch (err) {
+            console.error({ err });
+        }
     };
 
-    if (imageURl && receivedEmail) {
-      sendEmail();
-    }
-  }, [imageURl]);
+    useEffect(() => {
 
-  return (
-    <div>
-      <Toaster />
-      {!imageURl ? (
-        <main className="flex min-h-screen w-full flex-col items-center justify-center px-4 md:p-8 relative">
-          <div
-            className={`${styles.toggleButton}`}
-            onClick={handleEmailReceived}
-          >
-            <div
-              className={`${receivedEmail ? styles.receivedEmail : ""}`}
-            ></div>
-          </div>
-          <Head>
-            <title>Avatar Generator</title>
-          </Head>
-          <header className="mb-8 flex w-full flex-col items-center justify-center">
-            <h1 className="text-4xl font-bold">Avatar Generator</h1>
-            <p className="opacity-60">
-              Upload a picture of yourself and generate your avatar
-            </p>
-          </header>
-          <form
-            method="POST"
-            className="flex w-full flex-col md:w-[60%]"
-            onSubmit={(e) => handleSubmit(e)}
-          >
-            <label htmlFor="email">Email Address</label>
-            <input
-              type="email"
-              required
-              className="mb-3 border-[1px] px-4 py-2"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
+        if (imageURl) {
+            setEmail("");
+            setGender("");
+            setUserPrompt("");
+            setSelectedFile("");
+        }
 
-            <label htmlFor="gender">Gender</label>
-            <select
-              className="mb-4 rounded border-[1px] px-4 py-3"
-              name="gender"
-              id="gender"
-              value={gender}
-              onChange={(e) => setGender(e.target.value)}
-              required
-            >
-              <option value="">Select</option>
-              <option value="male">Male</option>
-              <option value="female">Female</option>
-            </select>
-            <UploadButton
-              endpoint="imageUploader"
-              onClientUploadComplete={(res) => {
-                // Do something with the response.
-                setSelectedFile(res[0].url);
-                toast.success("Image uploaded successfully!");
-              }}
-              onUploadError={(error: Error) => {
-                toast.error(`ERROR! ${error.message}`);
-              }}
-            />
-            <label htmlFor="prompt">
-              Add custom prompt for your avatar
-              <span className="opacity-60">(optional)</span>
-            </label>
-            <textarea
-              rows={4}
-              className="w-full border-[1px] p-3"
-              name="prompt"
-              id="prompt"
-              value={userPrompt}
-              placeholder="Copy image prompts from https://lexica.art"
-              onChange={(e) => setUserPrompt(e.target.value)}
-            />
-            <button
-              type="submit"
-              onClick={() => setClick(true)}
-              className="mt-5 rounded bg-blue-500 px-6 py-4 text-lg text-white hover:bg-blue-700"
-            >
-              {click ? (
-                <BeatLoader size={8} color="white" />
-              ) : (
-                <span>Generate Image</span>
-              )}
-            </button>
-          </form>
-        </main>
-      ) : (
-        <div className="min-h-screen w-full flex flex-col items-center justify-center">
-          <Image
-            src={imageURl}
-            width={200}
-            height={200}
-            alt="image"
-            className="mb-10"
-          />
-          <h2 className="font-bold text-3xl mb-2">Thank you! 🌟</h2>
-          <p className="mb-4 text-center">
-            {emailSent ? (
-              <span>Your avatar has been sent to your email address</span>
+        const sendEmail = async () => {
+            const emailRes = await fetch(`/api/send`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    email: email,
+                    imageURl: imageURl,
+                }),
+            });
+
+            const emailResponse = await emailRes.json();
+
+            if (emailResponse.error) {
+                console.error({ emailResponse });
+                return toast.error(emailResponse.error);
+            }
+
+            setEmailSent(true);
+
+            toast.success("Email sent successfully!");
+        }
+
+        if (imageURl && receivedEmail) {
+            sendEmail();
+        }
+
+    }, [imageURl]);
+
+    return (
+        <div>
+            <Toaster />
+            {!imageURl ? (
+                <main className="flex min-h-screen w-full flex-col items-center justify-center px-4 md:p-8 relative">
+                    <div className={`${styles.toggleButton} ${receivedEmail ? styles.receivedEmail : ""}`} onClick={() => setReceivedEmail(!receivedEmail)}>
+                        <div>
+                        </div>
+                    </div>
+                    <Head>
+                        <title>Avatar Generator</title>
+                    </Head>
+                    <header className="mb-8 flex w-full flex-col items-center justify-center">
+                        <h1 className="text-4xl font-bold">Avatar Generator</h1>
+                        <p className="opacity-60">
+                            Upload a picture of yourself and generate your avatar
+                        </p>
+                    </header>
+                    <form
+                        method="POST"
+                        className="flex w-full flex-col md:w-[60%]"
+                        onSubmit={(e) => handleSubmit(e)}
+                    >
+                        <label htmlFor="email">Email Address</label>
+                        <input
+                            type="email"
+                            required
+                            className="mb-3 border-[1px] px-4 py-2"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+
+                        <label htmlFor="gender">Gender</label>
+                        <select
+                            className="mb-4 rounded border-[1px] px-4 py-3"
+                            name="gender"
+                            id="gender"
+                            value={gender}
+                            onChange={(e) => setGender(e.target.value)}
+                            required
+                        >
+                            <option value="">Select</option>
+                            <option value="male">Male</option>
+                            <option value="female">Female</option>
+                        </select>
+                        <UploadButton
+                            endpoint="imageUploader"
+                            onClientUploadComplete={(res) => {
+                                // Do something with the response.
+                                setSelectedFile(res[0].url);
+                                toast.success("Image uploaded successfully!");
+                            }}
+                            onUploadError={(error: Error) => {
+                                toast.error(`ERROR! ${error.message}`);
+                            }}
+                        />
+                        <label htmlFor="prompt">
+                            Add custom prompt for your avatar
+                            <span className="opacity-60">(optional)</span>
+                        </label>
+                        <textarea
+                            rows={4}
+                            className="w-full border-[1px] p-3"
+                            name="prompt"
+                            id="prompt"
+                            value={userPrompt}
+                            placeholder="Copy image prompts from https://lexica.art"
+                            onChange={(e) => setUserPrompt(e.target.value)}
+                        />
+                        <button
+                            type="submit"
+                            onClick={() => setClick(true)}
+                            className="mt-5 rounded bg-blue-500 px-6 py-4 text-lg text-white hover:bg-blue-700"
+                        >
+                            {click ? (
+                                <BeatLoader size={8} color='white' />
+                            ) : (
+                                <span>
+                                    Generate Image
+                                </span>
+                            )}
+                        </button>
+                    </form>
+                </main>
             ) : (
-              <span>
-                {receivedEmail && <BeatLoader size={8} color="black" />}
-              </span>
+                <div className='min-h-screen w-full flex flex-col items-center justify-center'>
+                    <Image src={imageURl} width={200} height={200} alt="image" className="mb-10" />
+                    <h2 className='font-bold text-3xl mb-2'>Thank you! 🌟</h2>
+                    <p className='mb-4 text-center'>
+                        {emailSent ? (
+                            <span>
+                                Your avatar has been sent to your email address
+                            </span>
+                        ) : (
+                            <span>
+                              {receivedEmail && <BeatLoader size={8} color='black' />}
+                            </span>
+                        )}
+                    </p>
+                    <Link
+                        href='/'
+                        className='bg-blue-500 text-white px-4 py-3 rounded hover:bg-blue-600'
+                    >
+                        Generate another
+                    </Link>
+                </div>
             )}
-          </p>
-          <Link
-            href="/"
-            className="bg-blue-500 text-white px-4 py-3 rounded hover:bg-blue-600"
-          >
-            Generate another
-          </Link>
         </div>
-      )}
-    </div>
-  );
+    );
 }
