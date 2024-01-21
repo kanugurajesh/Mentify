c6d04601af71e99f6d28f67a4e82ce7126eb7fc6
kanugurajesh <kanugurajesh3@gmail.com>
2024-01-21 17:16:40 +0530

    Making changes in prompt engineering

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -7,11 +7,12 @@ export async function POST(req: NextRequest) {
   // get prompt field from the request body
   const reqBody = await req.json();
   const { userPrompt } = reqBody;
+  const prompt = "you are a mental health professional. you are talking to a patient who is suffering from depression. you want to help them feel better. so answer the following questions: " + userPrompt;
   const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY as string);
   const model = genAI.getGenerativeModel({ model: "gemini-pro", generationConfig: { maxOutputTokens: 400 }});
 
   try {
-    const result = await model.generateContent(userPrompt);
+    const result = await model.generateContent(prompt);
     const response = await result.response;
     const text = response.text();
     return NextResponse.json({
