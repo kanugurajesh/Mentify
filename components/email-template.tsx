3a3f5dfb26fdeb8f3d7b8de45b9f638880da08a8
kanugurajesh <kanugurajesh3@gmail.com>
2024-01-21 12:51:40 +0530

    Handling submit

--- components/email-template.tsx
+++ components/email-template.tsx
@@ -5,6 +5,12 @@ interface EmailTemplateProps {
   email: string
 }
 
+interface ContactEmailTemplateProps {
+  email: string,
+  subject: string,
+  message: string
+}
+
 export const EmailTemplate: React.FC<Readonly<EmailTemplateProps>> = ({
   imageURl, email
 }) => (
@@ -12,5 +18,16 @@ export const EmailTemplate: React.FC<Readonly<EmailTemplateProps>> = ({
     <h1>Here is your image</h1>
     <h2>Email is {email}</h2>
     <img src={imageURl} alt="image" />
+  </div>
+);
+
+export const ContactEmailTemplate: React.FC<Readonly<ContactEmailTemplateProps>> = ({
+  email, subject, message
+}) => (
+  <div>
+    <h1>You got a message from Mentify</h1>
+    <h2>Email is {email}</h2>
+    <h2>Subject is {subject}</h2>
+    <h2>Message is {message}</h2>
   </div>
 );
